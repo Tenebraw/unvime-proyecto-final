b36d4491a75d5fc6aedb41fd0e8a6029b02c52b5
Woodenheart <marcosadriansh@gmail.com>
2020-11-25 18:39:28 -0300

    Modificacion logo. Modificacion menu. Correccion detalles index. Pagina register.html terminada. Pagina resume.html terminada. Modificacion css. Estructura gral pagina create.html

--- site/public/js/menu.js
+++ site/public/js/menu.js
@@ -1,14 +1,50 @@
+//Menu 
 const $btnMenu = document.querySelector('#btn-menu');
 
 const $options = document.querySelector('#options-menu');
 
 const $contentToHidden = document.querySelector('.main-send');
 
-function desplegarMenu(){
+function desplegarMenu() {
     $options.classList.toggle('desplegado');
     $options.classList.toggle('oculto')
     $contentToHidden.classList.toggle('oculto')
 }
 
 $btnMenu.addEventListener("click", desplegarMenu);
+///////////////////////
 
+//Resume
+//Menu Encuestas. Ocultar y Desplegar.
+const $btnSurvey = document.querySelector('#btn-menu-survey');
+const $menuSurvey = document.querySelector('#survey-menu');
+
+function desplegarMenuSurvey() {
+    $menuSurvey.classList.toggle('desplegado-survey');
+    $menuSurvey.classList.toggle('ocult');
+}
+$btnSurvey.addEventListener("click", desplegarMenuSurvey);
+
+//Menu Encuestas 2. A corregir. Es ineficaz que existan dos funciones identicas.
+const $btnSurvey2 = document.querySelector('#btn-menu-survey2');
+const $menuSurvey2 = document.querySelector('#survey-menu2');
+
+function desplegarMenuSurvey2() {
+    $menuSurvey2.classList.toggle('desplegado-survey');
+    $menuSurvey2.classList.toggle('ocult');
+}
+$btnSurvey2.addEventListener("click", desplegarMenuSurvey2);
+
+//Cerrar Menu al hacer click en el boton cerrar del sub-menu Encuestas.
+function cerrarMenu() {
+    $menuSurvey.classList.toggle('ocult');
+    $menuSurvey.classList.toggle('desplegado-survey');
+}
+
+//Funcion identica a la anterior. Mal!
+function cerrarMenu2() {
+    $menuSurvey2.classList.toggle('ocult');
+    $menuSurvey2.classList.toggle('desplegado-survey');
+}
+
+//////////////////////////////////////////
