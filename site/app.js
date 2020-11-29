1db638e21b794dda09ec7ca622342fc633898f83
Woodenheart <marcosadriansh@gmail.com>
2020-11-29 07:39:54 -0300

    Diseño gral primera version terminado. Create, end, resume, index, register, send. Correcciones diseño Css, html y js.

--- site/app.js
+++ site/app.js
@@ -5,30 +5,30 @@ app.use(express.static('public'));
 
 //Las rutas:
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/resume', (req, res)=>{
+app.get('/resume', (req, res) => {
     res.sendFile(__dirname + '/views/resume.html');
 })
 
-app.get('/create', (req, res)=>{
+app.get('/create', (req, res) => {
     res.sendFile(__dirname + '/views/create.html');
 })
 
-app.get('/send', (req, res)=>{
+app.get('/send', (req, res) => {
     res.sendFile(__dirname + '/views/send.html');
 })
 
-app.get('/end', (req, res)=>{
+app.get('/end', (req, res) => {
     res.sendFile(__dirname + '/views/end.html');
 })
 
-app.get('/register', (req, res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Servidor en el puerto 3000');
 })
--- site/public/js/create.js
+++ site/public/js/create.js
@@ -12,6 +12,32 @@ function nuevaPregunta() {
     const newid = Date.now().toString(36) + Math.random().toString(36).substr(2);
     newPreguntadiv.setAttribute('id', newid);
     document.querySelector('#form-encuesta').insertBefore(newPreguntadiv, menuextra);
+}
 
+///// Muestra los tipos de preguntas.
+function tipoPreguntas() {
+    document.querySelector("#myDropdown").classList.toggle("show");
+}
 
+/////// Muestra las opciones del campo "Nombre de la encuesta"
+let menu = document.querySelector('.btn');
+let extra = document.querySelector('.hid');
+
+menu.addEventListener("click", function() {
+    extra.classList.toggle('mostrar');
+
+});
+
+/// Posibilita que se cierre las opciones del campo "Nombre de la encuesta" si se hace click fuera de ellas.
+window.onclick = function(event) {
+    if (!event.target.matches('.btn')) {
+        let dropdowns = document.querySelectorAll('.hid');
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('mostrar')) {
+                openDropdown.classList.remove('mostrar');
+            }
+        }
+    }
 }
