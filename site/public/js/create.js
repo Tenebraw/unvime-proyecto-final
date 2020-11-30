7c9a50bba1e0437d9730c5512c4fd3529fe8e4cb
Woodenheart <marcosadriansh@gmail.com>
2020-11-30 07:22:13 -0300

    Correciones menores en create.js. Metodos de seleccion. Ahora el submenu tipo de pregunta, aparece en cada nueva pregunta que se incorpora al formulario. Aun asi, faltan corregir su funcionamiento fluido.

--- site/public/js/create.js
+++ site/public/js/create.js
@@ -1,25 +1,31 @@
 function nuevaPregunta() {
-    const menuextra = document.querySelector('.menu-extra');
+    //const menuextra = document.querySelector('.menu-extra');
     const newPreguntadiv = document.createElement('div');
     const newTextarea = document.createElement('textarea');
     const newLabel = document.createElement('label');
+    newLabel.className = 'labelclass';
     newLabel.innerHTML = 'Introduzca su nueva pregunta...';
     newPreguntadiv.className = 'content-create';
     newTextarea.className = 'question';
 
     newPreguntadiv.appendChild(newTextarea);
     newPreguntadiv.appendChild(newLabel)
+        //La siguiente linea permite que cada nueva pregunta tenga su propio id.
     const newid = Date.now().toString(36) + Math.random().toString(36).substr(2);
     newPreguntadiv.setAttribute('id', newid);
-    document.querySelector('#form-encuesta').insertBefore(newPreguntadiv, menuextra);
+    //document.querySelector('#form-encuesta').insertBefore(newPreguntadiv, menuextra);
+    const formultimo = document.querySelector('#form-encuesta').lastElementChild;
+    document.querySelector('#form-encuesta').insertBefore(newPreguntadiv, formultimo);
+    MenuTipo();
+
 }
 
 ///// Muestra los tipos de preguntas.
 function tipoPreguntas() {
-    document.querySelector("#myDropdown").classList.toggle("show");
+    document.querySelector('.dropdown-content').classList.toggle("show");
 }
 
-/////// Muestra las opciones del campo "Nombre de la encuesta"
+/////// Muestra las opciones del campo "Nombre de la encuesta". A corregir.
 let menu = document.querySelector('.btn');
 let extra = document.querySelector('.hid');
 
@@ -40,4 +46,31 @@ window.onclick = function(event) {
             }
         }
     }
+}
+
+//Aun en proceso. Si bien se crea el correspondiente menutipo en cada nueva pregunta, al hacer click solo muestra las opciones de la primera. 
+function MenuTipo() {
+
+    const test = document.createElement('div');
+    test.className = 'test';
+    test.innerHTML = `<div class="dropdown">
+    <button onclick="tipoPreguntas(this)" class="dropbtn">Tipo</button>
+    <i class="fas fa-sort-down icon"></i>
+    <div class="dropdown-content">
+        <ul>
+            <li><a href="">Multiple-choice (simple)</a></li>
+            <li><a href="">Multiple-choice (multiple)</a></li>
+            <li><a href="">Menu Desplegable</a></li>
+            <li><a href="">Escala Opinion</a></li>
+            <li><a href="">Pregunta Abierta</a></li>
+            <li><a href="">Formulario de Contacto</a></li>
+        </ul>
+    </div>
+</div>`;
+
+    const lastID = document.querySelectorAll('#form-encuesta .content-create');
+    const lastIDprueba = lastID[lastID.length - 1];
+    const idlast = lastIDprueba.id;
+    document.querySelector(`#${idlast}`).appendChild(test);
+
 }
